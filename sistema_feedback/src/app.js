4da11ea204f7f7d78cc2948dcf1da94369be7d60
matheus <matheus.defaveri@escola.pr.gov.br>
2026-03-17 21:29:18 -0300

    aviao

--- sistema_feedback/src/app.js
+++ sistema_feedback/src/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+app.use(cors());
 
 require("./config/database");
 
@@ -7,5 +9,24 @@ const feedbackRoutes = require("./routes/feedbackRoutes");
 
 app.use(express.json());
 app.use("/feedbacks", feedbackRoutes);
+
+
+app.post("/login", (req, res) => {
+    const { email, senha } = req.body;
+
+    const db = require("./config/database");
+
+    const sql = "SELECT * FROM usuarios WHERE email = ? AND senha = ?";
+
+    db.query(sql, [email, senha], (err, result) => {
+        if (err) return res.status(500).json(err);
+
+        if (result.length > 0) {
+            res.json({ message: "Login efetuado" });
+        } else {
+            res.status(401).json({ message: "algo deu errado" });
+        }
+    });
+});
 
 module.exports = app;
--- sistema_feedback/src/config/database.js
+++ sistema_feedback/src/config/database.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
     host: "127.0.0.1",
     user: "root",
-    password: "admin",
+    password: "",
     database: "sistema_feedback",
     port:  3306
 });
